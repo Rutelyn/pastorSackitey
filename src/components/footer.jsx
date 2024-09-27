f775f65b07e069e104483004e8015cd07f36e279
Harika <ruth.agbozo.93@gmail.com>
2024-09-27 13:15:00 +0000

    color

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div>
+        <div className="bg-teal-600">
             <div>Logo andcopyright </div>
             <div>contact information, including social media <br /> and website and email links</div>
         </div>
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,6 +1,6 @@
 const LandingPage = () => {
     return (
-        <div>
+        <div className="bg-yellow-400">
             The landing page will cover an executive summary <br /> of a sort and will be put in at least two sections
         </div>
     );
